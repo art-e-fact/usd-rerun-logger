e24e614fcc57fa531832e76efcb34e5885e090b7
Andras Polgar <azazdeaz@gmail.com>
2025-12-30 16:51:53 +0100

    Fix timeline control in LogRerun

--- src/usd_rerun_logger/env_logger.py
+++ src/usd_rerun_logger/env_logger.py
@@ -2,28 +2,21 @@
 
 from __future__ import annotations
 
-import gc
-import os
 from collections.abc import Callable
-from copy import deepcopy
 from pathlib import Path
 from typing import Any, Generic, SupportsFloat
 
-import numpy as np
 import rerun as rr
 
 import gymnasium as gym
-from gymnasium import error, logger
 from gymnasium.core import ActType, ObsType, RenderFrame
-from gymnasium.error import DependencyNotInstalled, InvalidProbability
 
-from .util import get_recording_stream
 from .isaac_lab_logger import IsaacLabRerunLogger
 
-from gymnasium.wrappers.rendering import RecordVideo
 __all__ = [
     "LogRerun",
 ]
+
 
 class LogRerun(
     gym.Wrapper[ObsType, ActType, ObsType, ActType],
@@ -42,19 +35,19 @@ class LogRerun(
     The ``episode_trigger`` should return ``True`` on the episode when logging should start.
     The ``step_trigger`` should return ``True`` on the n-th environment step that the logging should be started, where n sums over all previous episodes.
     If neither :attr:`episode_trigger` nor ``step_trigger`` is passed, a default ``episode_trigger`` will be employed, i.e. :func:`capped_cubic_video_schedule`.
-    This function starts a video at every episode that is a power of 3 until 1000 and then every 1000 episodes.
+    This function starts a recording at every episode that is a power of 3 until 1000 and then every 1000 episodes.
     By default, the logging will be stopped once reset is called.
     However, you can also create recordings of fixed length (possibly spanning several episodes)
     by passing a strictly positive value for ``recording_length``.
 
     No vector version of the wrapper exists.
 
-    Examples - Run the environment for 50 episodes, and save the video every 10 episodes starting from the 0th:
+    Examples - Run the environment for 50 episodes, and save the recording every 10 episodes starting from the 0th:
         >>> import os
         >>> import gymnasium as gym
         >>> env = gym.make("LunarLander-v3", render_mode="rgb_array")
         >>> trigger = lambda t: t % 10 == 0
-        >>> env = RecordVideo(env, video_folder="./save_videos1", episode_trigger=trigger, disable_logger=True)
+        >>> env = LogRerun(env, save_path="./save_rerun1", episode_trigger=trigger)
         >>> for i in range(50):
         ...     termination, truncation = False, False
         ...     _ = env.reset(seed=123)
@@ -62,15 +55,15 @@ class LogRerun(
         ...         obs, rew, termination, truncation, info = env.step(env.action_space.sample())
         ...
         >>> env.close()
-        >>> len(os.listdir("./save_videos1"))
+        >>> len(os.listdir("./save_rerun1"))
         5
 
-    Examples - Run the environment for 5 episodes, start a recording every 200th step, making sure each video is 100 frames long:
+    Examples - Run the environment for 5 episodes, start a recording every 200th step, making sure each recording is 100 frames long:
         >>> import os
         >>> import gymnasium as gym
         >>> env = gym.make("LunarLander-v3", render_mode="rgb_array")
         >>> trigger = lambda t: t % 200 == 0
-        >>> env = RecordVideo(env, video_folder="./save_videos2", step_trigger=trigger, recording_length=100, disable_logger=True)
+        >>> env = LogRerun(env, save_path="./save_rerun2", step_trigger=trigger, recording_length=100)
         >>> for i in range(5):
         ...     termination, truncation = False, False
         ...     _ = env.reset(seed=123)
@@ -79,14 +72,14 @@ class LogRerun(
         ...         obs, rew, termination, truncation, info = env.step(env.action_space.sample())
         ...
         >>> env.close()
-        >>> len(os.listdir("./save_videos2"))
+        >>> len(os.listdir("./save_rerun2"))
         2
 
     Examples - Run 3 episodes, record everything, but in chunks of 1000 frames:
         >>> import os
         >>> import gymnasium as gym
         >>> env = gym.make("LunarLander-v3", render_mode="rgb_array")
-        >>> env = RecordVideo(env, video_folder="./save_videos3", recording_length=1000, disable_logger=True)
+        >>> env = LogRerun(env, save_path="./save_rerun3", recording_length=1000)
         >>> for i in range(3):
         ...     termination, truncation = False, False
         ...     _ = env.reset(seed=123)
@@ -94,36 +87,31 @@ class LogRerun(
         ...         obs, rew, termination, truncation, info = env.step(env.action_space.sample())
         ...
         >>> env.close()
-        >>> len(os.listdir("./save_videos3"))
+        >>> len(os.listdir("./save_rerun3"))
         2
-
-    Change logs:
-     * v0.25.0 - Initially added to replace ``wrappers.monitoring.VideoRecorder``
     """
 
     def __init__(
         self,
         env: gym.Env[ObsType, ActType],
+        logged_envs: int | list[int] = 0,
         recording_stream: rr.RecordingStream | None = None,
         save_path: Path | str | None = None,
         episode_trigger: Callable[[int], bool] | None = None,
         step_trigger: Callable[[int], bool] | None = None,
         recording_length: int = 0,
-        gc_trigger: Callable[[int], bool] | None = lambda episode: True,
     ):
-        """Wrapper records videos of rollouts.
+        """Wrapper records Rerun logs of rollouts.
 
         Args:
             env: The environment that will be wrapped
-            video_folder (str): The folder where the recordings will be stored
+            logged_envs: The indices of the environments to log.
+            recording_stream: The Rerun recording stream to use. If ``None``, a new stream will be created.
+            save_path: The path where the Rerun recording will be saved. If ``None``, the recording will not be saved to disk.
             episode_trigger: Function that accepts an integer and returns ``True`` iff a recording should be started at this episode
             step_trigger: Function that accepts an integer and returns ``True`` iff a recording should be started at this step
             recording_length (int): The length of recorded episodes. If 0, entire episodes are recorded.
                 Otherwise, snippets of the specified length are captured
-            name_prefix (str): Will be prepended to the filename of the recordings
-            fps (int): The frame per second in the video. Provides a custom video fps for environment, if ``None`` then
-                the environment metadata ``render_fps`` key is used if it exists, otherwise a default value of 30 is used.
-            gc_trigger: Function that accepts an integer and returns ``True`` iff garbage collection should be performed after this episode
         """
         gym.utils.RecordConstructorArgs.__init__(
             self,
@@ -133,9 +121,18 @@ class LogRerun(
         )
         gym.Wrapper.__init__(self, env)
 
+        # Check if the environment has a scene
+        if not hasattr(self.env.unwrapped, "scene"):
+            raise ValueError((
+                "Cannot use LogRerun wrapper: the environment does not have a 'scene' attribute. "
+                "Are you sure this is an Isaac Lab based environment?"
+            ))
+
+        self.scene = self.env.unwrapped.scene
 
         self.logger = IsaacLabRerunLogger(
             scene=env.unwrapped.scene,
+            logged_envs=logged_envs,
             recording_stream=recording_stream,
             save_path=save_path,
             application_id=env.spec.id if env.spec is not None else "env",
@@ -148,15 +145,26 @@ class LogRerun(
 
         self.episode_trigger = episode_trigger
         self.step_trigger = step_trigger
-        self.gc_trigger = gc_trigger
-        self.recording_length: int = recording_length if recording_length != 0 else float("inf")
+        self.recording_length: int = (
+            recording_length if recording_length != 0 else float("inf")
+        )
 
         self.step_id = -1
         self.episode_id = -1
+        self.timeline_name: str | None = None
+        self.recorded_frames = 0
 
     def _capture_frame(self):
         """Capture a frame from the environment."""
+        if self.timeline_name is None:
+            return
+        timestamp = self.recorded_frames * self.logger.scene.physics_dt
+        print(f"Logging frame at time: {timestamp:.3f}s timeline: {self.timeline_name}")
+        self.logger.recording_stream.set_time(
+            timeline=self.timeline_name, duration=timestamp
+        )
         self.logger.log_scene()
+        self.recorded_frames += 1
 
     def reset(
         self, *, seed: int | None = None, options: dict[str, Any] | None = None
@@ -169,7 +177,7 @@ class LogRerun(
             self.stop_recording()
 
         if self.episode_trigger and self.episode_trigger(self.episode_id):
-            self.start_recording(f"episode-{self.episode_id}")
+            self.start_recording(f"episode_{self.episode_id}")
 
         self._capture_frame()
 
@@ -183,10 +191,12 @@ class LogRerun(
         self.step_id += 1
 
         if self.step_trigger and self.step_trigger(self.step_id):
-            self.start_recording(f"step-{self.step_id}")
+            self.start_recording(f"step_{self.step_id}")
 
         self._capture_frame()
 
+        if self.recorded_frames > self.recording_length:
+            self.stop_recording()
 
         return obs, rew, terminated, truncated, info
 
@@ -196,10 +206,15 @@ class LogRerun(
         if self.recording:
             self.stop_recording()
 
-    def start_recording(self, video_name: str):
+    def start_recording(self, timeline_name: str):
         """Start a new recording. If it is already recording, stops the current recording before starting the new one."""
+        self.timeline_name = timeline_name
         self.logger.recording_stream.reset_time()
+        print(f"Starting Rerun recording: {timeline_name}")
+        self.logger.recording_stream.set_time(timeline_name, timestamp=0.0)
 
     def stop_recording(self):
         """Stop current recording and saves the video."""
+        self.timeline_name = None
+        self.recorded_frames = 0
         self.logger.recording_stream.flush()
